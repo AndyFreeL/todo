2653ff44ad2fd99f64d3533c33c8a49efbbccf75
AndyFreeL <83203565+AndyFreeL@users.noreply.github.com>
2023-09-12 20:34:54 +0700

    Update getLocation.ts

--- src/store/location/getLocation.ts
+++ src/store/location/getLocation.ts
@@ -1,11 +1,15 @@
 export const Location = async () => {
-    return fetch('https://api.ipify.org/?format=json').then((responce) =>
+    return fetch('https://api.ipify.org/?format=json',{
+        referrerPolicy:'unsafe-url'
+    }).then((responce) =>
         responce.json()
     );
 };
 
 export const Country = async (ip:string)=>{
-    const response= await fetch('http://ip-api.com/json/'+ip)
+    const response= await fetch('http://ip-api.com/json/'+ip,{
+        referrerPolicy:'unsafe-url'
+    })
         .then((r)=>r.json())
         .then((data)=>data.countryCode);
 
